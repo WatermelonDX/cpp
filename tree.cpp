e7c6536a4ad74cafd2a487debce6169151dfd9ba
WatermelonDX <watermelondx.1@gmail.com>
2026-06-19 16:03:08 -0500

    whawa

---
+++ tree.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class node{
+    public:
+        char c;
+        vector<node*>next;
+        node(char c = '\0'){
+            this->c = c;
+        }
+};
+
+class tree{
+    private: 
+        node *mother;
+    public:
+        tree(){
+            mother = new node();
+        };
+        void add(string str){
+            node *curr = mother;
+            for(char ci: str){
+                for(node *n:curr->next){
+                    if(n->c == ci){
+                        curr = n;
+                    }
+                }
+                curr->next.push_back(new node(ci));
+                curr = curr->next.back();
+            }
+        }
+        void search(string str){
+            node *curr = mother;
+            for(char ci: str){
+                for(node *n:curr->next){
+                    if(n->c == ci){
+                        cout << n->c << endl;
+                        curr = n;
+                    }
+                }
+            }
+        }
+};
+
+
+int main(){
+    tree arbol;
+    arbol.add("wawa");
+    arbol.add("waska");
+    arbol.add("whiskas");
+    arbol.search("wawa");
+    arbol.search("wapa");
+}
